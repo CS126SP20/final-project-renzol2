dd603919dca55d72fefbc5833ed4d781d93d804d
Renzo Ledesma <renzol2@illinois.edu>
2020-04-22 22:07:42 -0500

    Implement instrument selection from the available nodes in Cinder-Stk, removing generator if needed

--- apps/covid_sonif_app.cc
+++ apps/covid_sonif_app.cc
@@ -62,7 +62,93 @@ void CovidSonificationApp::HandleInstrumentsSelected() {
   if (instrument_) instrument_->disconnectAll();
   if (generator_)  generator_->disconnectAll();
 
+  generator_enum_selection_ = 0;  // set generator to "none"
+  generator_ = nullptr;
+  generator_gain_->setValue(0);
 
+  // Get the name of selected instrument and notify user
+  std::string name = instrument_enum_names_.at(instrument_enum_selection_);
+  CI_LOG_I("Selecting instrument '" << name << "'" );
+
+  // Set instrument accordingly
+  auto ctx = cinder::audio::master();
+  if (name == "BandedWG") {
+    auto instr = ctx->makeNode<cistk::BandedWGNode>();
+    instr->setPreset(3);  // preset: 'Tibetan Bowl'
+    instrument_ = instr;
+  } else if (name == "BlowBotl") {
+    instrument_ = ctx->makeNode<cistk::BlowBotlNode>();
+  } else if (name == "BlowHole") {
+    instrument_ = ctx->makeNode<cistk::BlowHoleNode>();
+  } else if (name == "Bowed") {
+    instrument_ = ctx->makeNode<cistk::BowedNode>();
+  } else if( name == "Brass" ) {
+    instrument_ = ctx->makeNode<cistk::BrassNode>();
+  } else if( name == "Clarinet" ) {
+    instrument_ = ctx->makeNode<cistk::ClarinetNode>();
+  } else if( name == "Drummer" ) {
+    instrument_ = ctx->makeNode<cistk::DrummerNode>();
+  } else if( name == "Flute" ) {
+    instrument_ = ctx->makeNode<cistk::FluteNode>();
+  } else if( name == "Mandolin" ) {
+    instrument_ = ctx->makeNode<cistk::MandolinNode>();
+  } else if (name == "Mesh2D") {
+    auto instr = ctx->makeNode<cistk::Mesh2DNode>();
+    instr->setDecay(0.9f);
+    instrument_ = instr;
+  } else if( name == "ModalBar" ) {
+    auto instr = ctx->makeNode<cistk::ModalBarNode>();
+    instr->setPreset( 1 ); // preset: 'Vibraphone'
+    instrument_ = instr;
+  } else if( name == "Moog" ) {
+    instrument_ = ctx->makeNode<cistk::MoogNode>();
+  } else if( name == "Plucked" ) {
+    instrument_ = ctx->makeNode<cistk::PluckedNode>();
+  } else if( name == "Resonate" ) {
+    instrument_ = ctx->makeNode<cistk::ResonateNode>();
+  } else if( name == "Saxofony" ) {
+    instrument_ = ctx->makeNode<cistk::SaxofonyNode>();
+  } else if( name == "Shakers" ) {
+    instrument_ = ctx->makeNode<cistk::ShakersNode>();
+  } else if( name == "Simple" ) {
+    instrument_ = ctx->makeNode<cistk::SimpleNode>();
+  } else if( name == "Sitar" ) {
+    instrument_ = ctx->makeNode<cistk::SitarNode>();
+  } else if( name == "StifKarp" ) {
+    instrument_ = ctx->makeNode<cistk::StifKarpNode>();
+  } else if( name == "VoicForm" ) {
+    auto instr = ctx->makeNode<cistk::VoicFormNode>();
+    if( ! instr->setPhoneme( "ooo" ) ) {
+      CI_LOG_W( "phoneme not found" );
+    }
+    instr->quiet();
+    instrument_ = instr;
+  } else if( name == "BeeThree" ) {
+    instrument_ = ctx->makeNode<cistk::BeeThreeNode>();
+  } else if( name == "FMVoices" ) {
+    instrument_ = ctx->makeNode<cistk::FMVoicesNode>();
+  } else if( name == "HevyMetl" ) {
+    instrument_ = ctx->makeNode<cistk::HevyMetlNode>();
+  } else if( name == "PercFlut" ) {
+    instrument_ = ctx->makeNode<cistk::PercFlutNode>();
+  } else if( name == "Rhodey" ) {
+    instrument_ = ctx->makeNode<cistk::RhodeyNode>();
+  } else if( name == "TubeBell" ) {
+    instrument_ = ctx->makeNode<cistk::TubeBellNode>();
+  } else if( name == "Wurley" ) {
+    instrument_ = ctx->makeNode<cistk::WurleyNode>();
+  } else if( name == "Whistle" ) {
+    instrument_ = ctx->makeNode<cistk::WhistleNode>();
+  } else {
+    CI_LOG_E("Unknown instrument name");
+    CI_ASSERT_NOT_REACHABLE();
+  }
+
+  // Set effect, if applicable
+  if (effect_) instrument_ >> effect_ >> master_gain_;
+  else         instrument_ >> master_gain_;
+
+  master_gain_ >> ctx->getOutput();
 }
 
 void CovidSonificationApp::HandleGeneratorSelected() {}
@@ -73,7 +159,7 @@ void CovidSonificationApp::HandleInstrumentSpecificNote(
     const cinder::vec2& pos) {}
 
 void CovidSonificationApp::PrintAudioGraph() {
-  std::cout << cinder::audio::master()->printGraphToString() << std::endl;
+  CI_LOG_I("\n" << cinder::audio::master()->printGraphToString());
 }
 
 void CovidSonificationApp::SetupMasterGain() {
